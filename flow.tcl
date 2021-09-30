ece5f59f88587cc314f681eb1a723702f0a3eecb
nofal <ahmad.nofal@efabless.com>
2021-09-30 13:05:24 +0200

    EFIC-168: to be reverted

--- flow.tcl
+++ flow.tcl
@@ -213,10 +213,10 @@ proc run_non_interactive_mode {args} {
 
 
 	calc_total_runtime
+    puts_info [array get ::env]
 	save_state
 	generate_final_summary_report
 
-    puts_info [array get ::env]
 	puts_success "Flow Completed Without Fatal Errors."
 }
 
